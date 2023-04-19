0940cc966cf7b4db2964262860f2304892b0aaff
yaofeil <53163171+cyprien0312@users.noreply.github.com>
2023-04-19 22:57:46 +1000

    add class nodes, and MCTS

--- agent/program.py
+++ agent/program.py
@@ -1,6 +1,8 @@
 # COMP30024 Artificial Intelligence, Semester 1 2023
 # Project Part B: Game Playing Agent
 
+import math
+import random
 from referee.game import \
     PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
 
@@ -45,3 +47,65 @@ class Agent:
             case SpreadAction(cell, direction):
                 print(f"Testing: {color} SPREAD from {cell}, {direction}")
                 pass
+
+class Node:
+    def __init__(self, state, parent=None, action=None):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.children = []
+        self.wins = 0
+        self.visits = 0
+
+    def add_child(self, child):
+        self.children.append(child)
+
+class MCTS:
+    def __init__(self, root_state, num_iterations=1000, exploration_parameter=math.sqrt(2)):
+        self.root = Node(root_state)
+        self.num_iterations = num_iterations
+        self.exploration_parameter = exploration_parameter
+
+    def search(self):
+        for _ in range(self.num_iterations):
+            selected_node = self.select_node(self.root)
+            result = self.rollout(selected_node.state)
+            self.backpropagate(selected_node, result)
+
+        best_child = self.best_child(self.root, 0)
+        return best_child.action
+
+    def select_node(self, node):
+        if not node.children:
+            return node
+        else:
+            return self.select_node(self.best_child(node, self.exploration_parameter))
+
+    def expand(self, node):
+        legal_actions = node.state.get_legal_actions()
+        for action in legal_actions:
+            child_state = node.state.copy().apply_action(action)
+            child_node = Node(child_state, node, action)
+            node.add_child(child_node)
+
+    def rollout(self, state):
+        while not state.is_terminal():
+            legal_actions = state.get_legal_actions()
+            random_action = random.choice(legal_actions)
+            state = state.copy().apply_action(random_action)
+
+        return state.get_result()
+
+    def backpropagate(self, node, result):
+        node.visits += 1
+        node.wins += result
+        if node.parent:
+            self.backpropagate(node.parent, 1 - result)
+
+    def best_child(self, node, exploration_parameter):
+        def uct(child):
+            return (child.wins / child.visits) + exploration_parameter * math.sqrt(
+                math.log(node.visits) / child.visits
+            )
+
+        return max(node.children, key=uct)
