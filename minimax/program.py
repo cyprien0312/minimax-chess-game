a77529418f5b38219c4c844bb4dd2b4cb32c5632
yaofeil <53163171+cyprien0312@users.noreply.github.com>
2023-05-02 04:08:50 +1000

    add IDDFS, heuristic sorting

--- minimax/program.py
+++ minimax/program.py
@@ -31,6 +31,8 @@ class Agent:
         """
         self._color = color
         self.game_state = Board()
+        self.node_explore = []
+        self.time_taken = []
         match color:
             case PlayerColor.RED:
                 print("Testing: I am playing as red")
@@ -43,20 +45,31 @@ class Agent:
         """
         match self._color:
             case PlayerColor.RED:
-                minimax = MiniMax(self.game_state, PlayerColor.RED, max_depth=3)
-                spawns, spreads = minimax.root.get_legal_actions()
-                return random.choice(spawns + spreads)
+                starttime = time.time()
+                minimax = MiniMax(self.game_state, PlayerColor.RED, max_depth=1)
+                minimax.generate_tree()
+                best_action = minimax.find_next_step()
+                endtime = time.time()
+                print('time costs this round = ', endtime - starttime)
+                actions = minimax.root.get_legal_actions()
+                return random.choice(actions)
+                #return best_action
             case PlayerColor.BLUE:
                 # This is going to be invalid... BLUE never spawned!
                 starttime = time.time()
                 minimax = MiniMax(self.game_state, PlayerColor.BLUE, max_depth=2)
                 minimax.generate_tree()
-                #minimax.print_tree()
+                #total_nodes = minimax.print_tree()
                 best_action = minimax.find_next_step()
                 endtime = time.time()
-                print('time costs this round = ', endtime - starttime)
+                #self.node_explore.append(total_nodes)
+                self.time_taken.append(endtime - starttime)
+                print('total time costs = ', sum(self.time_taken))
+                #print(self.node_explore)
+                #print(self.time_taken)
+
                 return best_action
-    
+
     def turn(self, color: PlayerColor, action: Action, **referee: dict):
         """
         Update the agent with the last player's action.
@@ -87,31 +100,40 @@ class Node:
         return self.state.game_over
     
     def get_legal_actions(self):
-        board = self.state
-        color = self.color
-        spawns = []
-        spreads = []
+        actions = []
         directions = [HexDir.Down, HexDir.DownLeft, HexDir.DownRight, HexDir.Up, HexDir.UpLeft, HexDir.UpRight]
         for cor in COORDINATES:
-            if board._cell_occupied(cor):
-                cellColor, cellPower = board[cor]
-                if cellColor == color:
+            if self.state._cell_occupied(cor):
+                cellColor, cellPower = self.state[cor]
+                if cellColor == self.color:
                     for direction in directions:
-                        spreads.append(SpreadAction(cor, direction))
+                        actions.append(SpreadAction(cor, direction))
             else:
-                spawns.append(SpawnAction(cor))
-        return spawns, spreads
+                actions.append(SpawnAction(cor))
+        return actions
     
     def evaluation(self):
         red_power = 0
         blue_power = 0
+        red_cells = 0
+        blue_cells = 0
+
         for cell in self.state._state.values():
             if cell.player == PlayerColor.BLUE:
                 blue_power += cell.power
-            else:
+                blue_cells += 1
+            elif cell.player == PlayerColor.RED:
                 red_power += cell.power
+                red_cells += 1
 
-        return blue_power - red_power
+        power_score = blue_power - red_power
+        cell_score = blue_cells - red_cells
+        # Assign weights to each factor according to their importance
+        power_weight = 2
+        cell_weight = 1
+
+        return (power_weight * power_score) + (cell_weight * cell_score)
+
     
 class MiniMax:
     def __init__(self, root_state, curr_color, max_depth = 3):
@@ -125,10 +147,10 @@ class MiniMax:
         if depth == 0 or node.is_terminal_node():
             return
         
-        spawns, spreads = node.get_legal_actions()
+        actions = node.get_legal_actions()
         next_color = _SWITCH_COLOR[node.color]
         #print(spawns + spreads)
-        for action in spawns + spreads:
+        for action in actions:
             child_state = deepcopy(node.state)
             child_state.apply_action(action)
             child_node = Node(child_state, next_color, node.level + 1, parent=node, action=action)
@@ -137,19 +159,45 @@ class MiniMax:
     
     def find_next_step(self):
         maximizing_player = self.root.color == PlayerColor.BLUE
-        best_value, best_action = self._minimax_alpha_beta(self.root, self.max_depth, float('-inf'), float('inf'), maximizing_player)
+        best_action = None
+        start_time = time.time()
+        time_limit = 5
+
+        for depth in range(1, self.max_depth + 1):
+            try:
+                current_value, current_action = self._minimax_alpha_beta(self.root, depth, float('-inf'), float('inf'), maximizing_player)
+                best_action = current_action
+            except TimeoutError:
+                break
+
+            # Check if the elapsed time exceeds the time limit
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= time_limit:
+                break
+
         return best_action
 
-    def _minimax_alpha_beta(self, node, depth, alpha, beta, maximizing_player):
-        if depth == 0 or node.is_terminal_node():
-            #print(node.evaluation())
-            return node.evaluation(), node.action
 
+    def heuristic(self, node: Node):
+        return node.evaluation()
+
+    def _minimax_alpha_beta(self, node, depth, alpha, beta, maximizing_player, start_time=None, time_limit=5):
+        if start_time is None:
+            start_time = time.time()
+
+        if depth == 0 or node.is_terminal_node():
+            return node.evaluation(), node.action
+        random.shuffle(node.children)
+        node.children.sort(key=self.heuristic, reverse=maximizing_player)
+        if time.time() - start_time >= time_limit:
+            raise TimeoutError("Time limit exceeded")
+
+        node.children.sort(key=self.heuristic, reverse=maximizing_player)
         if maximizing_player:
             max_value = float('-inf')
             best_action = None
             for child in node.children:
-                value, action = self._minimax_alpha_beta(child, depth - 1, alpha, beta, False)
+                value, action = self._minimax_alpha_beta(child, depth - 1, alpha, beta, False, start_time, time_limit)
                 if value > max_value:
                     max_value = value
                     best_action = child.action
@@ -161,7 +209,7 @@ class MiniMax:
             min_value = float('inf')
             best_action = None
             for child in node.children:
-                value, action = self._minimax_alpha_beta(child, depth - 1, alpha, beta, True)
+                value, action = self._minimax_alpha_beta(child, depth - 1, alpha, beta, True, start_time, time_limit)
                 if value < min_value:
                     min_value = value
                     best_action = child.action
@@ -169,8 +217,12 @@ class MiniMax:
                 if beta <= alpha:
                     break
             return min_value, best_action
+
+
     def print_tree(self):
-        self._print_tree_recursive(self.root, 0)
+        total_nodes = self._print_tree_recursive(self.root, 0)
+        print(f"Total nodes: {total_nodes}")
+        return total_nodes
 
     def _print_tree_recursive(self, node: Node, indent_level):
         indent = '  ' * indent_level
@@ -179,5 +231,8 @@ class MiniMax:
         else:
             print(f"{indent}Root (Level: {node.level}, Value: {node.evaluation()})")
 
+        node_count = 1  # Count the current node
         for child in node.children:
-            self._print_tree_recursive(child, indent_level + 1)
+            node_count += self._print_tree_recursive(child, indent_level + 1)
+
+        return node_count
